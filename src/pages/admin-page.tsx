8ecee62d9c40240717520642886dd682dca8488f
MeeMeChu <tanakeet005@gmail.com>
2024-09-24 22:33:44 +0700

    📦 NEW: Edit new table User Management

--- src/pages/admin-page.tsx
+++ src/pages/admin-page.tsx
@@ -1,34 +1,52 @@
-import { FC } from 'react'
-import { Box, Container, Tooltip } from '@mui/material';
+import { FC, useState } from 'react'
+import { Box, Button, Container, InputAdornment, Paper, styled, TextField, Tooltip, Typography } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
 import { DataGrid, GridRowsProp, GridColDef, GridActionsCellItem } from '@mui/x-data-grid';
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
 
-const rows: GridRowsProp = [
-    { id: 1, col1: 'MeeMeChu', col2: 'Thanakrit', col3: "Yodmunee", col4: "6510210114@email.psu.ac.th", col5: "test" },
-    { id: 2, col1: 'DataGridPro', col2: 'is Awesome', col3: "test", col4: "test", col5: "test"  },
-    { id: 3, col1: 'MUI', col2: 'is Amazing', col3: "test", col4: "test", col5: "test"  },
+const initialRows : GridRowsProp = [
+    { id: 1, username: 'MeeMeChu', firstname: 'Thanakrit', lastname: "Yodmunee", email: "6510210114@email.psu.ac.th", role: "test" },
+    { id: 2, username: 'DataGridPro', firstname: 'is Awesome', lastname: "test", email: "test", role: "test"  },
+    { id: 3, username: 'MUI', firstname: 'is Amazing', lastname: "test", email: "test", role: "test"  },
+    { id: 4, username: 'MUI', firstname: 'is Amazing', lastname: "test", email: "test", role: "test"  },
+    { id: 5, username: 'MUI', firstname: 'is Amazing', lastname: "test", email: "test", role: "test"  },
+    { id: 6, username: 'MUI', firstname: 'is Amazing', lastname: "test", email: "test", role: "test"  },
+    { id: 7, username: 'MUI', firstname: 'is Amazing', lastname: "test", email: "test", role: "test"  },
+    { id: 8, username: 'MUI', firstname: 'is Amazing', lastname: "test", email: "test", role: "test"  },
+    { id: 9, username: 'MUI', firstname: 'is Amazing', lastname: "test", email: "test", role: "test"  },
+    { id: 10, username: 'MUI', firstname: 'is Amazing', lastname: "test", email: "test", role: "test"  },
+    { id: 11, username: 'MUI', firstname: 'is Amazing', lastname: "test", email: "test", role: "test"  },
+    { id: 12, username: 'MUI', firstname: 'is Amazing', lastname: "test", email: "test", role: "test"  },
+    { id: 13, username: 'MUI', firstname: 'is Amazing', lastname: "test", email: "test", role: "test"  },
+    { id: 14, username: 'MUI', firstname: 'is Amazing', lastname: "test", email: "test", role: "test"  },
+    { id: 15, username: 'MUI', firstname: 'is Amazing', lastname: "test", email: "test", role: "test"  },
+    { id: 16, username: 'MUI', firstname: 'is Amazing', lastname: "test", email: "test", role: "test"  },
+    { id: 17, username: 'MUI', firstname: 'is Amazing', lastname: "test", email: "test", role: "test"  },
+    { id: 18, username: 'MUI', firstname: 'is Amazing', lastname: "test", email: "test", role: "test"  },
+    { id: 19, username: 'MUI', firstname: 'is Amazing', lastname: "test", email: "test", role: "test"  },
+    { id: 20, username: 'MUI', firstname: 'is Amazing', lastname: "test", email: "test", role: "test"  },
 ];
 
 const columns: GridColDef[] = [
     { 
-        field: 'col1', 
+        field: 'username', 
         headerName: 'Username', 
         width: 150 
     },
-    {   field: 'col2', 
+    {   field: 'firstname', 
         headerName: 'Firstname', 
         width: 150 
     },
-    {   field: 'col3', 
+    {   field: 'lastname', 
         headerName: 'Lastname', 
         width: 150 
     },
-    {   field: 'col4', 
+    {   field: 'email', 
         headerName: 'Email', 
         width: 270
     },
-    {   field: 'col5', 
+    {   field: 'role', 
         headerName: 'Role', 
         width: 150 
     },
@@ -63,10 +81,58 @@ const columns: GridColDef[] = [
 
 
 const AdminPage : FC = () => {
+
+    const [searchText, setSearchText] = useState<string>('');
+    const [filteredData, setFilteredData] = useState(initialRows);
+
+    const [paginationModel, setPaginationModel] = useState({
+        page: 0,
+        pageSize: 10,
+    });
+    
+    // กรองข้อมูลตามข้อความที่พิมพ์
+    const handleSearch = (e : React.ChangeEvent<HTMLInputElement>) => {
+        const searchTerm = e.target.value;
+        setSearchText(searchTerm);
+
+        const filtered = initialRows.filter((row) =>
+            row.firstname.toLowerCase().includes(searchText.toLowerCase())
+        );
+
+        setFilteredData(filtered);
+    };
+
     return (
-        <Container>
-            <Box sx={{ height: 350, width : '100%'}}>
-                <DataGrid rows={rows} columns={columns} />
+        <Container maxWidth="lg" >
+            <Box sx={{ my : 1.5 }}>
+                <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
+                    <Typography variant='h5'>User Management</Typography>
+                    <Button variant="contained">Add new User</Button>
+                </Box>
+                <Paper elevation={5} sx={{ mt : 2, p: 2 }}>
+                    <TextField 
+                        variant="outlined" 
+                        placeholder='Search...'
+                        size="small"
+                        value={searchText}
+                        onChange={handleSearch}
+                        slotProps={{
+                            input: {
+                                endAdornment: <InputAdornment position="end"><SearchIcon /></InputAdornment>
+                            } 
+                        }}
+                    />
+                    <Box sx={{width : '100%', mt: 2}}>
+                        <DataGrid 
+                            pagination
+                            rows={filteredData} 
+                            columns={columns} 
+                            pageSizeOptions={[10]}
+                            paginationModel={paginationModel}
+                            onPaginationModelChange={setPaginationModel}
+                        />
+                    </Box>
+                </Paper>
             </Box>
         </Container>
     );
