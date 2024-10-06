d51f4112d861a568314ef558ed5a18db154843f5
tamamiwakimura <tamamiwakimura@gmail.com>
2024-10-06 20:46:03 +0700

    update exam

--- src/pages/exam/edit-exam.page.tsx
+++ src/pages/exam/edit-exam.page.tsx
@@ -1,14 +1,95 @@
-import { FC } from 'react'
+import { FC , useEffect, useState} from 'react'
 import { useParams } from 'react-router-dom'
 import { Box, Container, styled, Typography } from '@mui/material';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import Grid from '@mui/material/Grid2';
 import CloudUploadIcon from '@mui/icons-material/CloudUpload';
-const EditExamPage : FC = () => {
+import { useNavigate } from 'react-router-dom';
 
-    const { id } = useParams<{ id : string}>();
-    const VisuallyHiddenInput = styled('input')({
+type Exams = {
+  examID : string,
+  subID:string,
+  examFileURL: string,
+  examDetail: string,
+  examRoom : string,
+  examStdCount: string,
+  examStartDate: string,
+  examEndDate : string,
+  examCreateAt : string,
+  examUpdateAt: string,
+  subName?: string,
+  subStatus?: string
+}
+
+const EditExamPage: FC = () => {
+  const { id } = useParams<{ id: string }>();
+  const [formData, setFormData] = useState<Exams>({
+    examID : '',
+    subID:'',
+    examFileURL: '',
+    examDetail: '',
+    examRoom : '',
+    examStdCount: '',
+    examStartDate: '',
+    examEndDate : '',
+    examCreateAt : '',
+    examUpdateAt: '',
+    subName: '',
+    subStatus: ''
+  });
+  const navigate = useNavigate();
+
+  console.log(formData);
+  useEffect(() => {
+    const fetchExamData = async () => {
+        try {
+            const response = await fetch(`http://localhost:8000/api/exams/${id}`);
+            if (!response.ok) {
+                throw new Error('Failed to fetch exam data')
+            }
+            const result = await response.json();
+            
+            setFormData(result);
+        } catch (error) {
+            console.error("Error : ", error);
+        }
+    }
+
+    fetchExamData();
+},[]);
+
+const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setFormData({
+        ...formData,
+        [name] : value
+    })
+}
+
+const handdleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    try {
+        const response = await fetch(`http://localhost:8000/api/exams/edit/${id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(formData),
+        });
+
+        if (response.ok) {
+            console.log('Data edit successfully!');
+        } else {
+            console.error('Failed to edit data.');
+        }
+
+        navigate('/exam');
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+  const VisuallyHiddenInput = styled('input')({
         clip: 'rect(0 0 0 0)',
         clipPath: 'inset(50%)',
         height: 1,
@@ -24,7 +105,7 @@ const EditExamPage : FC = () => {
           <Box 
             sx={{ my: 1 ,mt:8}}
           >
-            <Typography variant="h5" fontWeight='bold'>ADD exam</Typography>
+            <Typography variant="h5" fontWeight='bold'>ADD exam </Typography>
           </Box>
           <Box
             sx={{
@@ -32,66 +113,132 @@ const EditExamPage : FC = () => {
               boxShadow: '0px 8px 24px rgba(149, 157, 165, 0.2)'
             }}
           >
-            <Grid container spacing={3}>
-                {/* บรรทัดที่1 */}
-                <Grid size={3}>
-                  <Typography variant="h5" sx={{fontSize:16 , px:1}}>รหัสวิชา</Typography>
-                  <TextField required id="outlined-required"placeholder="Enter subject ID"  fullWidth size="small"/>
-                </Grid>
-                <Grid size={9}>
-                  <Typography variant="h5" sx={{fontSize:16, px:1}}>ชื่อวิชา</Typography>
-                  <TextField required id="outlined-required"placeholder="Enter subject Name" fullWidth size="small"/>
-                </Grid>
-                {/* บรรทัดที่2 */}
-                <Grid size={4}>
-                  <Typography variant="h5" sx={{fontSize:16, px:1}}>ตอน</Typography>
-                  <TextField required id="outlined-required"placeholder="0" fullWidth size="small"/>
-                </Grid>
-                <Grid size={4}>
-                  <Typography variant="h5" sx={{fontSize:16, px:1}}>จำนวนนักศึกษาที่เข้าสอบ</Typography>
-                  <TextField required id="outlined-required"placeholder="0" fullWidth size="small"/>
-                </Grid>
-                <Grid size={4}>
-                  <Typography variant="h5" sx={{fontSize:16, px:1}}>ห้องสอบ</Typography>
-                  <TextField required id="outlined-required"placeholder="room" fullWidth size="small"/>
-                </Grid>
-                {/* บรรทัดที่3 */}
-                <Grid size={12}>
-                  <Typography variant="h5" sx={{fontSize:16, px:1}}>รายละเอียดจ่าหน้าซอง</Typography>
-                  <TextField required id="outlined-required"placeholder="" fullWidth />
-                </Grid>
-                {/* บรรทัดที่3 */}
-                <Grid size={12}>
-                  <Typography variant="h5" sx={{fontSize:16, px:1,mb:2}}>อัพโหลดไฟลล์ข้อสอบ</Typography>
-                  <Button
-                    component="label"
-                    role={undefined}
-                    variant="contained"
-                    tabIndex={-1}
-                    startIcon={<CloudUploadIcon />}
-                    
-                    >
-                        Upload files
-                    <VisuallyHiddenInput
-                        type="file"
-                        onChange={(event) => console.log(event.target.files)}
-                        multiple
+            <form onSubmit={handdleSubmit}>
+              <Grid container spacing={3}>
+                  {/* บรรทัดที่1 */}
+                  <Grid size={3}>
+                    <Typography variant="h5" sx={{fontSize:16 , px:1}}>รหัสวิชา</Typography>
+                    <TextField 
+                    required id="outlined-required"
+                    placeholder="Enter subject ID"  
+                    fullWidth 
+                    size="small"
+                    name="subID"
+                    value={formData?.subID}
+                    onChange={handleChange}
+                    disabled
                     />
-                    </Button>
-                </Grid>
-            </Grid>
+                  </Grid>
+                  <Grid size={7}>
+                    <Typography variant="h5" sx={{fontSize:16, px:1}}>ชื่อวิชา</Typography>
+                    <TextField 
+                    required 
+                    id="outlined-required"
+                    placeholder="Enter subject Name" 
+                    fullWidth 
+                    size="small"
+                    name="subName"
+                    value={formData.subName}
+                    onChange={handleChange}
+                    disabled
+
+                    /> 
+                    </Grid>
+                      <Grid size={2}>
+                    <Typography variant="h5" sx={{fontSize:16, px:1}}>ตอน</Typography>
+                    <TextField 
+                    required 
+                    id="outlined-required"
+                    placeholder="0" 
+                    fullWidth 
+                    size="small"
+                    name="subID"
+                    value={formData?.subID}
+                    onChange={handleChange}
+                    />
+                    </Grid>
+
+                  {/* บรรทัดที่2 */}
+                  <Grid size={2.5}>
+                    <Typography variant="h5" sx={{fontSize:16, px:1}}>จำนวนนักศึกษาที่เข้าสอบ</Typography>
+                    <TextField 
+                    required 
+                    id="outlined-required"
+                    placeholder="0" 
+                    fullWidth 
+                    size="small"
+                    name="examStdCount"
+                    value={formData?.examStdCount}
+                    onChange={handleChange}
+                    />
+                  </Grid>
+                  <Grid size={2.5}>
+                    <Typography variant="h5" sx={{fontSize:16, px:1}}>ห้องสอบ</Typography>
+                    <TextField 
+                    required 
+                    id="outlined-required"
+                    placeholder="room" 
+                    fullWidth size="small"
+                    name="examRoom"
+                    value={formData?.examRoom}
+                    onChange={handleChange}
+                    />
+                  </Grid>
+                  <Grid size={3.5}>
+                    <Typography variant="h5" sx={{fontSize:16, px:1}}>เวลาที่เริ่มสอบ</Typography>
+                    <TextField 
+                    required 
+                    id="outlined-required"
+                    placeholder="room" 
+                    fullWidth size="small" 
+                    type='time'
+                    name="examStartDate"
+                    value={formData?.examStartDate}
+                    onChange={handleChange}
+                    />
+                  </Grid>
+                  <Grid size={3.5}>
+                    <Typography variant="h5" sx={{fontSize:16, px:1}}>เวลาที่สิ้นสุด</Typography>
+                    <TextField required id="outlined-required"placeholder="room" fullWidth size="small" type='time'/>
+                  </Grid>
+                  {/* บรรทัดที่3 */}
+                  <Grid size={12}>
+                    <Typography variant="h5" sx={{fontSize:16, px:1}}>รายละเอียดจ่าหน้าซอง</Typography>
+                    <TextField required id="outlined-required"placeholder="" fullWidth />
+                  </Grid>
+                  {/* บรรทัดที่3 */}
+                  <Grid size={12}>
+                    <Typography variant="h5" sx={{fontSize:16, px:1,mb:2}}>อัพโหลดไฟลล์ข้อสอบ</Typography>
+                    <Button
+                      component="label"
+                      role={undefined}
+                      variant="contained"
+                      tabIndex={-1}
+                      startIcon={<CloudUploadIcon />}
+                      
+                      >
+                          Upload files
+                      <VisuallyHiddenInput
+                          type="file"
+                          onChange={(event) => console.log(event.target.files)}
+                          multiple
+                      />
+                      </Button>
+                  </Grid>
+              </Grid>
 
              {/* ปุ่มกดนะจ้ะ */}
             <Grid container spacing={3} sx={{display: 'flex', flexDirection:'row',justifyContent:'flex-end',my:6}}>
-             {/* ปุ่มกด1 */}
-              <Grid size={2}>
-                <Button variant="contained" fullWidth color="error">ยกเลิก</Button>
-              </Grid>
-             {/* ปุ่มกด2 */}
-              <Grid size={2}>
-                <Button variant="contained" fullWidth>ยืนยัน</Button>
-              </Grid>
+                {/* ปุ่มกด1 */}
+                <Grid size={2}>
+                  <Button variant="contained" fullWidth color="error">ยกเลิก</Button>
+                </Grid>
+                {/* ปุ่มกด2 */}
+                <Grid size={2}>
+                  <Button variant="contained" fullWidth type='submit'>ยืนยัน</Button>
+                </Grid>
             </Grid>
+            </form>
           </Box>    
         </Container>
     )
--- src/pages/exam/exam-page.tsx
+++ src/pages/exam/exam-page.tsx
@@ -1,120 +1,158 @@
-import { FC } from "react"
+import { FC,useCallback, useEffect, useState } from "react"
 import Container from '@mui/material/Container';
-import { Box,Typography , IconButton } from "@mui/material";
-import Grid from '@mui/material/Grid2';
-import { DataGrid, GridColDef } from '@mui/x-data-grid';
+import { Box,Typography , IconButton,Tooltip } from "@mui/material";
+import { DataGrid, GridColDef, GridValueGetter ,GridActionsCellItem} from '@mui/x-data-grid';
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
 import Button from '@mui/material/Button';
 import { useNavigate } from 'react-router-dom';
+import dayjs from "dayjs";
 
-
-
-const columns: GridColDef[] = [
-    { 
-        field: 'subjectName', 
-        headerName: 'ชื่อวิชา', 
-        type:'string',
-        width: 200,
-        headerAlign: 'center',
-        align: 'center'
-    },
-    {
-        field: 'subjectId',
-        headerName: 'รหัสวิชา',
-        type:'string',
-        width: 200,
-        headerAlign: 'center',
-        align: 'center'
-    },
-    {
-        field: 'dateAddSubject',
-        headerName: 'วันที่เพิ่มข้อสอบ',
-        width: 150,
-        headerAlign: 'center',
-        align: 'center'
-    },
-    {
-        field: 'dateEditSubject',
-        headerName: 'วันที่แก้ไขข้อสอบ',
-        width: 150,
-        editable: true,
-        headerAlign: 'center',
-        align: 'center'
-    },
-    {
-        field: 'status',
-        headerName: 'สถานะข้อสอบ',
-        sortable: false,
-        width: 200,
-        headerAlign: 'center',
-        align: 'center',
-        renderCell: (params) => {
-            let color = '';
-            if (params.value === 'ยังไม่ส่งข้อสอบ') {
-                color = 'red';
-            } else if (params.value === 'รอแก้ไข') {
-                color = '#D5A600';
-            } else if (params.value === 'จัดพิมพ์เสร็จสิ้น') {
-                color = 'green';
-            }
-            else if(params.value === 'ส่งข้อสอบแล้ว'){
-                color = '#0033CC';
-            }
-            return (
-                <Typography sx={{ color ,
-                    textAlign: 'center',
-                    my: 1.5,
-                }}>
-                    {params.value}
-                </Typography>
-            );
-        }
-    },
-    {
-        field: 'action',
-        headerName: 'Action',
-        sortable: false,
-        type:'actions',
-        width: 150,
-        headerAlign: 'center',
-        align: 'center',
-        renderCell: (params) => {
-            if (params.row.status === 'ยังไม่ส่งข้อสอบ' || params.row.status === 'จัดพิมพ์เสร็จสิ้น'){
-                return null;
-            }
-            return (
-                <>
-                    <IconButton
-                        color="primary"
-                        onClick={() => { alert(`Edit ${params.row.subjectName}`); }}
-                    >
-                        <EditIcon />
-                    </IconButton>
-                    <IconButton
-                        color="error"
-                        onClick={() => { alert(`Do you want to delete ${params.row.subjectName} ?`); }}
-                    >
-                        <DeleteIcon />
-                    </IconButton>
-                </>
-            );
-        }
-    },
-];
-
-const rows = [
-    { id: 1, subjectName: 'Internet of thing', subjectId: '344-323', dateAddSubject: '12/10/2567' , dateEditSubject: '12/12/2567' , status: 'จัดพิมพ์เสร็จสิ้น'},
-    { id: 2, subjectName: 'DataComputing', subjectId: '344-323', dateAddSubject: '12/10/2567' , dateEditSubject: '12/12/2567' , status: 'ส่งข้อสอบแล้ว'},
-    { id: 3, subjectName: 'SoftwareEnginearing', subjectId: '344-341', dateAddSubject: '12/10/2567' , dateEditSubject: '12/12/2567' , status: 'รอแก้ไข'},
-    { id: 4, subjectName: 'IDEA TO ENTREPRENEURSHIP', subjectId: '460-001 ', dateAddSubject: '12/10/2567' , dateEditSubject: '12/12/2567' , status: 'ยังไม่ส่งข้อสอบ'},
-    { id: 5, subjectName: 'PRINCIPLES OF AI', subjectId: '344-361', dateAddSubject: '12/10/2567' , dateEditSubject: '12/12/2567' , status: 'ส่งข้อสอบแล้ว'},
-    { id: 6, subjectName: 'Info', subjectId: '344-211', dateAddSubject: '12/10/2567' , dateEditSubject: '12/12/2567' , status: 'จัดพิมพ์เสร็จสิ้น'},
-];
+type Exams = {
+    examID : string,
+    subID:string,
+    examFileURL: string,
+    examDetail: string,
+    examRoom : string,
+    examStdCount: string,
+    examStartDate: string,
+    examEndDate : string,
+    examCreateAt : string,
+    examUpdateAt: string,
+    subName?: string,
+    subStatus?: string
+}
 
 const ExamPage : FC = () => {
     const navigate = useNavigate();
+    const [examData, setExamData] = useState<Exams[]>([]);
+    const [refresh, setRefresh] = useState<boolean>(false);
+    
+    console.log(examData);
 
+
+    
+    useEffect(() => {
+        const fetchExamsData = async () => {
+            try {
+                const response = await fetch('http://localhost:8000/api/exams');
+                if (!response.ok) {
+                    throw new Error('Failed to fetch exams data')
+                }
+                const result = await response.json();
+
+                const exam = result.map((value: any) => ({
+                    ...value,
+                    subName: value.subjects.subName,
+                    subStatus: value.subjects.subStatus
+                }));
+    
+                setExamData(exam);
+            } catch (error) {
+                console.error("Error : ", error);
+            }
+        }
+    
+        fetchExamsData();
+    },[refresh]);
+
+    const columns: GridColDef[] = [
+        { 
+            field: 'subName', 
+            headerName: 'ชื่อวิชา', 
+            width: 200,
+            headerAlign: 'center',
+            align: 'center',
+        },
+        {
+            field: 'subID',
+            headerName: 'รหัสวิชา',
+            type:'string',
+            width: 200,
+            headerAlign: 'center',
+            align: 'center'
+        },
+        {
+            field: 'examCreateAt',
+            headerName: 'วันที่เพิ่มข้อสอบ',
+            width: 150,
+            headerAlign: 'center',
+            align: 'center',
+            renderCell(param) {
+                return (
+                    <>{`${param?.row?.examCreateAt ? dayjs(param?.row?.examCreateAt).format('DD/MM/YYYY') : null}`}</>
+                );
+            }
+        },
+        {
+            field: 'examUpdateAt',
+            headerName: 'วันที่แก้ไขข้อสอบ',
+            width: 150,
+            editable: true,
+            headerAlign: 'center',
+            align: 'center',
+            renderCell(param) {
+                return (
+                    <>{`${param?.row?.examUpdateAt ? dayjs(param?.row?.examUpdateAt).format('DD/MM/YYYY') : null}`}</>
+                );
+            }
+        },
+        {
+            field: 'subStatus',
+            headerName: 'สถานะข้อสอบ',
+            sortable: false,
+            width: 200,
+            headerAlign: 'center',
+            align: 'center',
+            renderCell: (params) => {
+                let color = '';
+                if (params.value === 'ยังไม่ส่งข้อสอบ') {
+                    color = 'red';
+                } else if (params.value === 'รอแก้ไข') {
+                    color = '#D5A600';
+                } else if (params.value === 'จัดพิมพ์เสร็จสิ้น') {
+                    color = 'green';
+                }
+                else if(params.value === 'ส่งข้อสอบแล้ว'){
+                    color = '#0033CC';
+                }
+                return (
+                    <Typography sx={{ color ,
+                        textAlign: 'center',
+                        my: 1.5,
+                    }}>
+                        {params.value}
+                    </Typography>
+                );
+            }
+        },
+        {
+            field: 'action',
+            headerName: 'Action',
+            sortable: false,
+            type:'actions',
+            width: 150,
+            headerAlign: 'center',
+            align: 'center',
+            renderCell: (params) => {
+                if (params.row.status === 'ยังไม่ส่งข้อสอบ' || params.row.status === 'จัดพิมพ์เสร็จสิ้น'){
+                    return null;
+                }
+                return (
+                    <>
+                    <Tooltip key={1} title="แก้ไขข้อมูล">
+                        <GridActionsCellItem
+                            icon={<EditIcon color='primary'/>}
+                            label="Edit"
+                            onClick={() => navigate('edit/' + params?.row?.examID)}
+                        />  
+                    </Tooltip>
+                    </>
+                );
+            }
+        },
+    ];
+    
     return (
         <Container sx={{mt:15}}>
             
@@ -132,7 +170,7 @@ const ExamPage : FC = () => {
                 <Box sx={{p: 5, boxShadow: '0px 8px 24px rgba(149, 157, 165, 0.2)'}}>
                         <DataGrid
                             sx={{boxShadow: 2}}
-                            rows={rows}
+                            rows={examData.map((item) => ({ id: item.examID, ...item })) || []}
                             columns={columns}
                             initialState={{
                                 pagination: {
