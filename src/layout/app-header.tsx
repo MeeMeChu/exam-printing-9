189339db4cf57d3ee577acabeedbd29dbab63a2a
MeeMeChu <tanakeet005@gmail.com>
2024-10-06 20:31:48 +0700

    📦 NEW: Create login page

--- src/layout/app-header.tsx
+++ src/layout/app-header.tsx
@@ -1,7 +1,32 @@
-import { AppBar, Box, Button, Toolbar, Typography } from "@mui/material"
-import { FC } from "react";
+import { AppBar, Box, IconButton, Menu, MenuItem, Toolbar, Typography } from "@mui/material"
+import { FC, useState } from "react";
+import { useAuth } from "../context/AuthContext";
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import { useNavigate } from "react-router-dom";
 
 const AppHeader : FC = () => {
+
+    const auth = useAuth();
+    const navigate = useNavigate();
+
+    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+
+    const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    const handleLogout = async () => {
+        try {
+            await auth?.logout();
+            navigate('/');
+        } catch (error) {
+            console.error('Error logging out:', error);
+        }
+    }
 
     return (
         <Box sx={{ flexGrow: 1 }}>
@@ -10,6 +35,37 @@ const AppHeader : FC = () => {
                     <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                         Exam Printing
                     </Typography>
+                    {auth?.userLoggedIn && (
+                        <div>
+                            <IconButton
+                                size="large"
+                                aria-label="account of current user"
+                                aria-controls="menu-appbar"
+                                aria-haspopup="true"
+                                onClick={handleMenu}
+                                color="inherit"
+                            >
+                                <AccountCircle />
+                            </IconButton>
+                            <Menu
+                                id="menu-appbar"
+                                anchorEl={anchorEl}
+                                anchorOrigin={{
+                                    vertical: 'top',
+                                    horizontal: 'right',
+                                }}
+                                keepMounted
+                                transformOrigin={{
+                                    vertical: 'top',
+                                    horizontal: 'right',
+                                }}
+                                open={Boolean(anchorEl)}
+                                onClose={handleClose}
+                            >
+                                <MenuItem onClick={handleLogout}>Logout</MenuItem>
+                            </Menu>
+                        </div>
+                    )}
                 </Toolbar>
             </AppBar>
         </Box>
